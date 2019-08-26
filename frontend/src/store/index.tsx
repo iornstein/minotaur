e68b3dc68dad07f96127d7cb6fbabb4fc4bea96b
iornstein <iornstein@pivotal.io>
2019-08-26 11:25:07 -0500

    Enable redux devtools

--- frontend/src/store/index.tsx
+++ frontend/src/store/index.tsx
@@ -1,9 +1,17 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import {reducer} from "./reducer";
 import createSagaMiddleware from 'redux-saga'
 
 export const sagaMiddleware = createSagaMiddleware();
 
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function configureStore() {
-    return createStore(reducer,  applyMiddleware(sagaMiddleware));
+    return createStore(reducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 }
